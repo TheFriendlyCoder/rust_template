03a1e25d09ea665a4d308c227834f354bc78c3dd
Kevin Phillips <thefriendlycoder@gmail.com>
2022-02-26 12:43:32 -0400

    treat lint warnings as errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::restriction)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+
 fn main() {
     let fubar = false;
     if fubar == false {
