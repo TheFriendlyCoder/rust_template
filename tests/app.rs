378f4151973ba4bae49615dd2e046c58cf97d3be
Kevin Phillips <thefriendlycoder@gmail.com>
2022-02-27 08:25:08 -0400

    added clap and unit test support

--- tests/app.rs
+++ tests/app.rs
@@ -1,3 +1,9 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::error::Error;
+
+type TestResult = Result<(), Box<dyn Error>>;
+
 #[test]
 fn hello_world() {
     let value = true;
@@ -8,4 +14,15 @@ fn hello_world() {
 #[should_panic]
 fn test_panic_condition() {
     panic!("Darn it");
+}
+
+#[test]
+fn usage() -> TestResult {
+    for flag in &["-h", "--help"] {
+        Command::cargo_bin("sample")?
+            .arg(flag)
+            .assert()
+            .stdout(predicate::str::contains("USAGE"));
+    }
+    Ok(())
 }
